135cf948f9a399c9c0beb3126d0c510948cabf7b
vendarasanm <beginner@gmail.com>
2023-11-19 11:31:45 +0530

    portfolio

---
+++ portfolio.php
@@ -0,0 +1,171 @@
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <style>
+      .first{
+
+        padding:60px;
+        margin-right:100px;
+        float:right;
+        font-size:25px;
+        font-family:Verdana;
+        
+      }
+      .first a:hover{
+        color:#FFEC83;
+      }
+      .first a{
+        
+        color:grey;
+        
+        font-weight:bold;
+        font-size:40px;
+        
+       
+      }
+
+      body{
+        
+        background-position:center;
+        background-size:cover;
+        background:url('hp.jpg');
+        background-repeat:no-repeat;
+        background-size:cover;
+        
+        
+      }
+      .third{
+        padding:700px 0px 0px 150px;
+      }
+      .fourth{
+        text-align:center;
+        margin-right:600px;
+        padding:100px;
+        
+      }
+      .fourth {
+        float:left;
+
+      }
+      .second img{
+        margin-top:100px;
+        margin-left:100px;
+        padding:30px;
+        height:500px;
+        width:100%;
+        border-radius:50%;
+        justify-content:center;
+        align-items:center;
+      }
+    
+   .fifth{
+    float:right;
+    margin-right:250px;
+    margin-top:120px;
+    
+    
+
+   }
+   .fifth button{
+    padding:20px;
+    color:grey;
+    font-size:20px;
+   }
+   .first div input{
+    width: 500px;
+    
+   }
+   .buttons button{
+    font-size:40px;
+    color:grey;
+    padding: 20px 30px ;
+   }
+   .buttons button:hover{
+    color:white;
+    background-color:black;
+   }
+  
+      
+     
+</style>
+
+
+
+    </head>
+    <body>
+    
+  <div class="first">
+    <nav>
+        <div style="padding:50px;margin-right:60px;color:black;background-color:grey;border-radius:50%;">
+        <input type="text" placeholder="Search">&nbsp&nbsp<button>SEARCH</button>
+        </div>
+         <br>
+         <div class="buttons">
+         <button type="button" onclick="profile()" class="btn btn-outline-dark">HOME</button>&nbsp&nbsp&nbsp
+         <button type="button" onclick="profile()" class="btn btn-outline-dark">ABOUT</button>&nbsp&nbsp&nbsp
+         <button type="button" onclick="profile()" class="btn btn-outline-dark">CONTENT</button><br><br>
+  </div>
+         <br><br>
+
+
+  <div style="color:grey";>
+        <h3>Graduated as a <h3>
+        <h1>MECHATRONICS ENGINEER</h1>
+        <hr>
+        <br>
+        <br>
+        <h1>KEY SKILLS</h1>
+        <hr>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JAVA</li>
+            <li>PHP</li>
+</ul>
+<br>
+<h1>PROJECTS</h1>
+<hr>
+   3D Printer Filament Maker - BTech
+
+</nav>
+  </div>
+    
+    
+  <div class="second">
+    <img src="file.jpeg" style="width:500px;float:left;" >
+  </div>
+   
+  <div class="third">
+   <h3 style="font-size:75px;color:#FFEC83;margin-left:650px;margin-bottom:60px;">Welcome to my profile!</h3>
+   
+    <h1 style="font-size:50px;color:grey;"><b>I'm M.VENDARASAN</b></h1>
+     <h3 style="font-size:30px;color:grey;"> <i> Junior Software Developer - Trainee <br>&nbsp &nbsp -Geedesk Technologies</i><h3><br>
+     <h1 style="font-family:cursive;color:grey;">---Eager to use my programming experience and communication skills to work as professional developer---</h1>
+    <div style="color:grey;"><hr></div>
+  </div>
+
+    <div class="fourth">
+        <table border=2px>
+    <tr>
+    <th style="font-family:cursive; font-size:40px;color:grey">Contact : 8428870034</th>
+    </tr>
+    <tr>
+    <th style="font-family:cursive; font-size:40px;color:grey">Email : vendarasan.m@geedesk.com</th>
+    </tr>
+    </table>
+    </div>
+
+    <div class="fifth">
+        <button type="button" onclick="profile()" class="btn btn-outline-dark">view to next page</button>
+        
+</div>
+
+<script>
+  function profile(){
+    alert ("In Progress");
+  }
+  </script>
+
+    </body>
+    
+</html>
